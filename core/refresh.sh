d75b2265298e0ba2b3a1093a5b60c514f0fbce13
Al Munnings <al.munnings@gmail.com>
2024-01-31 11:39:23 +1100

    Dep bump, core 10.2 bump

--- core/refresh.sh
+++ core/refresh.sh
@@ -4,6 +4,6 @@
 
 BRANCH=$1
 
-curl "https://git.drupalcode.org/project/drupal/-/raw/${BRANCH}/core/.eslintrc.json?inline=false" --output drupal/.eslintrc.json
-curl "https://git.drupalcode.org/project/drupal/-/raw/${BRANCH}/core/.prettierrc.json?inline=false" --output drupal/.prettierrc.json
-curl "https://git.drupalcode.org/project/drupal/-/raw/${BRANCH}/core/.stylelintrc.json?inline=false" --output drupal/.stylelintrc.json
+curl "https://raw.githubusercontent.com/drupal/drupal/${BRANCH}/core/.eslintrc.json?inline=false" --output drupal/.eslintrc.json
+curl "https://raw.githubusercontent.com/drupal/drupal/${BRANCH}/core/.prettierrc.json?inline=false" --output drupal/.prettierrc.json
+curl "https://raw.githubusercontent.com/drupal/drupal/${BRANCH}/core/.stylelintrc.json?inline=false" --output drupal/.stylelintrc.json
